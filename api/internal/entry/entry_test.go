47078a4b0ae7290d0321787ec4b4effa0d3aacee
kristiannissen <kristian.nissen@gmail.com>
2024-03-07 08:08:20 +0100

    Markdown parser

---
+++ api/internal/entry/entry_test.go
@@ -0,0 +1,70 @@
+package entry
+
+import (
+	"os"
+	"path/filepath"
+	"testing"
+)
+
+func setup() {
+	// log.Println("ENV", os.Getenv("BLOB_READ_WRITE_TOKEN"))
+}
+
+func TestMain(m *testing.M) {
+	setup()
+	c := m.Run()
+	os.Exit(c)
+}
+
+func TestGetEntry(t *testing.T) {
+	_, err := GetEntry("sample")
+
+	if err != nil {
+		t.Fatal(err)
+	}
+}
+
+func TestParseEntryData(t *testing.T) {
+	var err error
+	var b []byte
+	var wd string
+	var e Entry
+
+	wd, err = os.Getwd()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	p := filepath.Join(wd, "/../../_content/sample.md")
+
+	b, err = os.ReadFile(p)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	e, err = ParseEntryData(string(b))
+
+	// Test meta
+	// log.Println("Meta:", e.Meta)
+	if e.Meta[0].Key != "tags" {
+		t.Fatal("No tags")
+	}
+
+	// Test headline
+	// log.Println("Title:", e.Title)
+	if e.Title == "" {
+		t.Fatal("No title")
+	}
+
+	// Paragraphs
+	// log.Println("Paras:", e.Paragraphs)
+	if len(e.Paragraphs) == 0 {
+		t.Fatal("No paragraphs")
+	}
+
+	// Images
+	// log.Println("Images", e.Images)
+	if len(e.Images) == 0 {
+		t.Fatal("No images")
+	}
+}
