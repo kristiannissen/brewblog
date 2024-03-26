073d98500f83352bedf3fda734bfe488af7f8a5f
kristiannissen <kristian.nissen@gmail.com>
2024-03-26 09:20:19 +0100

    Updating template

--- api/_pkg/render/render.go
+++ api/_pkg/render/render.go
@@ -27,7 +27,32 @@ func RenderTemplate(b []byte) ([]byte, error) {
 	var tpl = `
 <!DOCTYPE html>
 <html>
-<body>{{.}}</body>
+<body data-page="post">
+<header class="container_header header_logo" next-page-hide>
+<h1 class="font_heading"><a href="/">Hansen<span class="font_amp">&</span>Nissen</a></h1>
+</header>
+<main class="container__content">
+	<article class="container__article column">
+		<div class="content__header content__navigation pl-1 pr-1">
+			<nav class="navigation__breadcrumb">
+				<ul class="breadcrumb__list">
+					<li class="breadcrumb__list_item"><a href="/">Hjem</a></li>
+					<li class="breadcrumb__list_item">Blog Post</li>
+				</ul>
+			</nav>
+			<h1 class="font_header mb-2">{{title}}</h1>
+		</div>
+					{{.}}
+	</template>
+</article>
+</main>
+<footer class="container__footer">
+	<div class="column">
+		<b>Stærke øl til svage mænd!</b>
+	</div>
+	<div class="column">2</div>
+</footer>
+</body>
 </html>
 `
 
